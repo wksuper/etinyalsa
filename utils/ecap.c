035e06e2d8134a6bf00b3561ca65b15f35dc7390
Kui Wang <wangkuisuper@hotmail.com>
2020-11-08 12:05:29 +0800

    ecap: Support full arguments
    Usage: ecap {file.wav} [-D card] [-d device]
                           [-c channels] [-r rate] [-b bits]
    		       [-p period_size] [-n n_periods]
                           [-e extended_buffer_ms]
    
    Signed-off-by: Kui Wang <wangkuisuper@hotmail.com>

--- utils/ecap.c
+++ utils/ecap.c
@@ -24,6 +24,30 @@
 #include <signal.h>
 #include <unistd.h>
 
+#define ID_RIFF 0x46464952
+#define ID_WAVE 0x45564157
+#define ID_FMT  0x20746d66
+#define ID_DATA 0x61746164
+
+#define FORMAT_PCM 1
+
+struct wav_header {
+	uint32_t riff_id;
+	uint32_t riff_sz;
+	uint32_t riff_fmt;
+	uint32_t fmt_id;
+	uint32_t fmt_sz;
+	uint16_t audio_format;
+	uint16_t num_channels;
+	uint32_t sample_rate;
+	uint32_t byte_rate;
+	uint16_t block_align;
+	uint16_t bits_per_sample;
+	uint32_t data_id;
+	uint32_t data_sz;
+};
+
+
 static int g_quit;
 
 static void sigint_handler(int sig)
@@ -35,67 +59,159 @@ static void sigint_handler(int sig)
 
 int main(int argc, char *argv[])
 {
-	int ret = 0;
-	printf("ecap version: %s\n", VERSION);
-	printf("klogging version: %s\n", KVERSION());
+	int ret = EXIT_FAILURE;
+	struct pcm *pcm = NULL;
+	struct pcm_config config;
+	struct epcm *epcm = NULL;
+	struct epcm_config econfig;
+	FILE *file = NULL;
+	struct wav_header header;
+	unsigned int card = 0;
+	unsigned int device = 0;
+	unsigned int channels = 2;
+	unsigned int rate = 48000;
+	unsigned int bits = 16;
+	unsigned int frames;
+	unsigned int period_size = 1024;
+	unsigned int period_count = 4;
+	enum pcm_format format = PCM_FORMAT_S16_LE;
+	size_t extended_buffer_ms = 0;
+	char c = -1;
+	char *buf = NULL;
+	size_t read_size = 0;
+	size_t read_frames = 0;
+	size_t total_frames = 0;
+
 	KLOG_SET_OPTIONS(KLOGGING_TO_STDOUT);
 	KLOG_SET_LEVEL(KLOGGING_LEVEL_VERBOSE);
 
-	struct epcm *epcm = NULL;
-	struct pcm *pcm = NULL;
+	KCONSOLE("ecap version: %s\n", VERSION);
+	KCONSOLE("klogging version: %s\n", KVERSION());
 
-	struct epcm_config econfig;
-	memset(&econfig, 0, sizeof(econfig));
-	econfig.ram_millisecs = 10/*seconds*/ * 1000;
+	if (argc < 2) {
+		KCONSOLE("Usage: ecap {file.wav} [-D card] [-d device] [-c channels] "
+		         "[-r rate] [-b bits] [-p period_size] [-n n_periods] "
+			 "[-e extended_buffer_ms]\n");
+		goto error;
+	}
 
-	struct pcm_config config;
+	file = fopen(argv[1], "wb");
+	if (!file) {
+		KLOGE("Unable to open %s\n", argv[1]);
+		goto error;
+	}
+
+	while ((c = getopt(argc, argv, "D:d:c:r:b:p:n:e:")) != -1) {
+		switch (c) {
+		case 'd':
+			device = atoi(optarg);
+			break;
+		case 'c':
+			channels = atoi(optarg);
+			break;
+		case 'r':
+			rate = atoi(optarg);
+			break;
+		case 'b':
+			bits = atoi(optarg);
+			break;
+		case 'D':
+			card = atoi(optarg);
+			break;
+		case 'p':
+			period_size = atoi(optarg);
+			break;
+		case 'n':
+			period_count = atoi(optarg);
+			break;
+		case 'e':
+			extended_buffer_ms = atoi(optarg);
+			break;
+		case '?':
+			KCONSOLE("Unknown option: %c\n", c);
+			goto error;
+		}
+	}
+
+	header.riff_id = ID_RIFF;
+	header.riff_sz = 0;
+	header.riff_fmt = ID_WAVE;
+	header.fmt_id = ID_FMT;
+	header.fmt_sz = 16;
+	header.audio_format = FORMAT_PCM;
+	header.num_channels = channels;
+	header.sample_rate = rate;
+	switch (bits) {
+	case 32:
+		format = PCM_FORMAT_S32_LE;
+		break;
+	case 24:
+		format = PCM_FORMAT_S24_LE;
+		break;
+	case 16:
+		format = PCM_FORMAT_S16_LE;
+		break;
+	default:
+		KCONSOLE("%u bits is not supported\n", bits);
+		goto error;
+	}
+	header.bits_per_sample = pcm_format_to_bits(format);
+	header.byte_rate = (header.bits_per_sample / 8) * channels * rate;
+	header.block_align = channels * (header.bits_per_sample / 8);
+	header.data_id = ID_DATA;
+
 	memset(&config, 0, sizeof(config));
-	config.channels = 2;
-	config.rate = 48000;
-	config.period_size = 1024;
-	config.period_count = 4;
-	config.format = PCM_FORMAT_S16_LE;
+	config.channels = channels;
+	config.rate = rate;
+	config.period_size = period_size;
+	config.period_count = period_count;
+	config.format = format;
 	config.start_threshold = 0;
 	config.stop_threshold = 0;
 	config.silence_threshold = 0;
 
-	FILE *f = fopen("cap.pcm", "wb");
+	memset(&econfig, 0, sizeof(econfig));
+	econfig.ram_millisecs = extended_buffer_ms;
 
-	if (!f) {
-		KLOGE("Unable to open cap.pcm\n");
-		ret = -1;
-		goto error;
-	}
+	fseek(file, sizeof(struct wav_header), SEEK_SET);
 
 	signal(SIGINT, sigint_handler);
 
-	epcm = epcm_open(0, 0, PCM_IN, &config, &econfig);
+	epcm = epcm_open(card, device, PCM_IN, &config, &econfig);
 	if (!epcm) {
 		KLOGE("Unable to epcm_open()\n");
-		ret = -1;
 		goto error;
 	}
 	pcm = epcm_base(epcm);
 
-	size_t size = pcm_frames_to_bytes(pcm, pcm_get_buffer_size(pcm));
-	char *buf = malloc(size);
+	read_frames = pcm_get_buffer_size(pcm);
+	read_size = pcm_frames_to_bytes(pcm, read_frames);
+	buf = malloc(read_size);
 	if (!buf) {
-		KLOGE("Failed to malloc(%d bytes)\n", size);
-		ret = -1;
+		KLOGE("Failed to malloc(%d bytes)\n", read_size);
 		goto error;
 	}
-
+	total_frames = 0;
 	while (!g_quit) {
-		if (epcm_read(epcm, buf, size) == 0) {
-			fwrite(buf, size, 1, f);
+		if (epcm_read(epcm, buf, read_size) == 0) {
+			fwrite(buf, read_size, 1, file);
+			total_frames += read_frames;
 		} else {
-			KLOGE("Error to read %u bytes\n", size);
+			KLOGE("Error to read %u bytes\n", read_size);
 		}
 	}
+
+        header.data_sz = total_frames * header.block_align;
+        header.riff_sz = header.data_sz + sizeof(header) - 8;
+        fseek(file, 0, SEEK_SET);
+        fwrite(&header, sizeof(struct wav_header), 1, file);
+
+	ret = EXIT_SUCCESS;
 
 error:
 	epcm_close(epcm);
 	free(buf);
-	fclose(f);
+	if (file)
+		fclose(file);
 	return ret;
 }
